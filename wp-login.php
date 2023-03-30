c4ae57ca2fd1218983ac8fd99b36f2ce62f21a9e
rahmaa <113582506+rahmaazs@users.noreply.github.com>
2023-03-30 15:04:25 +0700

    update head dan body GTM

--- wp-login.php
+++ wp-login.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N9GXZ9K');</script>
+<!-- End Google Tag Manager -->
+
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Log In &lsaquo; Pencucian Sarang Walet &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
